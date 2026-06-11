11fa6411b615041d99a6e6e71042b216ffeab79d
Yaswanth-Botsa <yaswanthkumarbotsha@gmail.com>
2026-06-11 16:02:10 +0530

    Fix title and remove welcome text

--- app.py
+++ app.py
@@ -6,25 +6,9 @@ from groq_service import get_groq_response
 
 @cl.on_chat_start
 async def start():
-
     await cl.Message(
-        content="""
-# UPSC Assistant
-
-Ask only UPSC-related questions.
-
-Examples:
-• What are Fundamental Rights?
-• Explain DPSP.
-• What is the Green Revolution?
-• Explain El Niño.
-
-Non-UPSC questions will return:
-Invalid Query
-"""
+        content="# Chat Bot for UPSC"
     ).send()
-
-
 @cl.on_message
 async def main(message: cl.Message):
 
